abdb60ee705b82c91adc731296e2edc2495b2638
Nick Chikovani <nikabika97@gmail.com>
2015-12-20 10:15:32 +0400

    fix

--- app.js
+++ app.js
@@ -22,61 +22,8 @@ function handler (req, res) {
 
 
 io.on('connection', function (socket) {
-    console.log("Connected!");
-  
-    socket.on('rpi.init', function(leds) {
-        
-        rpis.push(socket);
-
-        for (var i in leds) {
-            leds.push({
-                "pos":leds[i].pos,
-                "rpi":(rpis.length-1),
-                "index":leds[i].index
-            });
-        }
-
-        console.log(leds);
-        console.log(data);
-
-        console.log("Initialized Rpi with "+Object.keys(leds).length+" leds");
-    });
-
-    socket.on('disconnect', function() {
-        console.log('Got disconnect!');
-        var i = rpis.indexOf(socket);
-
-        if(i > -1) {
-            console.log("It was RPI disconnect");
-
-            for(var j = leds.length - 1; j>=0; j--) {
-                if(leds[j].rpi == i) {
-                    leds.splice(j, 1);
-                }
-            }
-
-            rpis.splice(i, 1);
-        }
-    });
 
     socket.on('capture.car', function(data) {
-        var minPos = data.pos;
-        var maxPos = minPos + 100;
-        
-        console.log("Captured car at "+data.pos);
-
-        for(var i in leds) {
-            
-            if(leds[i].pos > minPos && leds[i].pos < maxPos) {
-            
-                // rpis[leds[i].rpi].emit("light.up", {
-                //     "index": leds[i].index
-                // });
-
-                // console.log("Lighting up #"+i);
-            }
-        }
-
-
+        socket.emit("light.up");        
     });
 });
